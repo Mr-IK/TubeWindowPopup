edb313c9b54706fe04d8f664a23d0b8457e2696b
Mr-IK <178kazu.ironman@gmail.com>
2020-09-25 22:34:19 +0900

    Updated 1.5

--- ytbutton/grid_video.js
+++ ytbutton/grid_video.js
@@ -1,8 +1,19 @@
 (function () {
+  var isDark = false;
   const createQuickButton = (className) => {
     let quickButton = document.createElement('button')
-    quickButton.id = 'twp-button'
-    quickButton.className= className ? className : ''
+    var html = document.getElementsByTagName('html')[0]
+
+    if(html.getAttribute('dark') == 'true'){
+      isDark = true;
+    }
+
+    if(isDark){
+        quickButton.className = 'twp-button-dark'
+    }else{
+        quickButton.className = 'twp-button'
+    }
+
     quickButton.innerHTML = '動画をポップアップ'
     quickButton.onclick = () => {
       let videoHref = quickButton.parentElement.parentElement.querySelector('a').href
@@ -12,10 +23,36 @@
     return quickButton
   }
 
+  const checkDarkTheme = () => {
+    var html = document.getElementsByTagName('html')[0]
+    var isDark2 = false;
+
+    if(html.getAttribute('dark') == 'true'){
+      isDark2 = true;
+    }
+
+    if(!isDark2){
+      var getTwpButtons = document.getElementsByClassName('twp-button-dark');
+      if(getTwpButtons!=null){
+        Array.prototype.forEach.call(getTwpButtons, dismissable => {
+          dismissable.className = 'twp-button'
+        })
+      }
+    }else{
+      var getTwpButtons = document.getElementsByClassName('twp-button');
+      if(getTwpButtons!=null){
+        Array.prototype.forEach.call(getTwpButtons, dismissable => {
+          dismissable.className = 'twp-button-dark'
+        })
+      }
+    }
+  }
+
   const addQuickButton = () => {
+
     let dismissableList = document.querySelectorAll('div#dismissable')
     Array.prototype.forEach.call(dismissableList, dismissable => {
-      if (dismissable.querySelector('#twp-button') === null && !dismissable.className.includes('ytd-shelf-renderer')) {
+      if (dismissable.querySelector('.twp-button') === null && dismissable.querySelector('.twp-button-dark') === null && !dismissable.className.includes('ytd-shelf-renderer')) {
         let detail = dismissable.querySelector('div#buttons')
         const quickButton = createQuickButton()
         if (detail) {
@@ -27,12 +64,14 @@
 
   const observeContent = () => {
     addQuickButton()
+    checkDarkTheme()
     // オブザーバインスタンスを作成
     let contents = document.querySelectorAll('div#contents')
     for (content of contents) {
       if (content.getAttribute('observe') !== true) {
         const observer = new MutationObserver(() => {
           addQuickButton()
+          checkDarkTheme()
         })
         const config = {
           childList: true,
@@ -45,9 +84,15 @@
 
   observeContent()
   document.body.addEventListener('yt-page-data-updated', () => {
-    setTimeout(() => addQuickButton() , 200)
+    setTimeout(() => {
+      addQuickButton()
+      checkDarkTheme()
+    } , 200)
   })
   document.addEventListener('yt-service-request-completed', () => {
-    setTimeout(() => addQuickButton(), 200)
+    setTimeout(() => {
+      addQuickButton()
+      checkDarkTheme()
+    } , 200)
   })
 }())
